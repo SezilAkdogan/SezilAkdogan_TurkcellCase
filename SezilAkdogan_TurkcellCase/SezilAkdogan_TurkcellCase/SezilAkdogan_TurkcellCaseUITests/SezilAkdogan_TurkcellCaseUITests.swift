7f3d120e88b90dbac8d857b341cf8c36194467e8
Sezil Akdoğan <111681902+SezilAkdogan@users.noreply.github.com>
2023-07-21 12:45:48 +0300

    UITest written/ edited

--- SezilAkdogan_TurkcellCase/SezilAkdogan_TurkcellCase/SezilAkdogan_TurkcellCaseUITests/SezilAkdogan_TurkcellCaseUITests.swift
+++ SezilAkdogan_TurkcellCase/SezilAkdogan_TurkcellCase/SezilAkdogan_TurkcellCaseUITests/SezilAkdogan_TurkcellCaseUITests.swift
@@ -8,34 +8,57 @@
 import XCTest
 
 final class SezilAkdogan_TurkcellCaseUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    private var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
+        app.launchArguments.append("******** UITest ********")
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    override func tearDownWithError() throws {
+        // Not needed for this test case
+    }
+    
+    func test_search(){
+        let app = XCUIApplication()
+        app.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["2013-03-05"]/*[[".cells.staticTexts[\"2013-03-05\"]",".staticTexts[\"2013-03-05\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.navigationBars["Video Games"].buttons["Video Games"].tap()
+    }
+    
+    func test_navigate_home_to_detail() {
+        let app = XCUIApplication()
+        let collectionViewsQuery2 = app.collectionViews
+        let staticText = collectionViewsQuery2/*@START_MENU_TOKEN@*/.staticTexts["2012-08-21"]/*[[".cells.staticTexts[\"2012-08-21\"]",".staticTexts[\"2012-08-21\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        staticText.tap()
+        
+        let tabBar = app.tabBars["Tab Bar"]
+        let videoGamesButton = tabBar.buttons["Video Games"]
+        videoGamesButton.tap()
+        
+        let collectionViewsQuery = collectionViewsQuery2
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["2007-10-09"]/*[[".cells.staticTexts[\"2007-10-09\"]",".staticTexts[\"2007-10-09\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        
+        let left4Dead2StaticText = collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["Left 4 Dead 2"]/*[[".cells.staticTexts[\"Left 4 Dead 2\"]",".staticTexts[\"Left 4 Dead 2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        let loveButton = app.buttons["love"]
+        loveButton.tap()
+        
+        let favouriteButton = tabBar.buttons["Favourite"]
+        favouriteButton.tap()
+        favouriteButton.tap()
+        
+        videoGamesButton.tap()
+        videoGamesButton.tap()
+        
+        
     }
 }
